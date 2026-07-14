07877582f67cc95f2cc3efbe416da6da4cd1541d
ayushrajdev <ayushraj.dev10@gmail.com>
2026-07-14 20:19:43 +0530

    added frontend

--- Submission-Service/src/main.ts
+++ Submission-Service/src/main.ts
@@ -6,7 +6,9 @@ import { connectMongoDb } from './config/mongoDb.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   app.setGlobalPrefix('/api/v1');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
