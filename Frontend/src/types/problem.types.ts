14fb6b3d5239a9417070650d6986bdc73f920c05
ayushrajdev <ayushraj.dev10@gmail.com>
2026-07-19 19:12:56 +0530

    Refactor ProblemData type to include additional fields and remove unused properties

--- Frontend/src/types/problem.types.ts
+++ Frontend/src/types/problem.types.ts
@@ -1,4 +1,28 @@
+// export type ProblemData = {
+//     title: string;
+//     url: string;
+// }
+
+export type TestCase = {
+    _id: string;
+    input: string;
+    output: string;
+};
+
+export type CodeStub = {
+    _id: string;
+    language: string;
+    startSnippet: string;
+    midSnippet: string;
+    endSnippet: string;
+};
+
 export type ProblemData = {
+    _id: string;
     title: string;
-    url: string;
-}
+    description: string;
+    difficulty: "easy" | "medium" | "hard";
+    testCases: TestCase[];
+    editorial: string;
+    codeStubs: CodeStub[];
+};
